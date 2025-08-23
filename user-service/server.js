fddcd1d0b79d4d52180a3e1b13a45e6ed5550755
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-08-24 02:52:37 +0530

    Setup User-service

--- user-service/server.js
+++ user-service/server.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const User = require('./user.modal.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,12 +13,14 @@ app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-// mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/task-management', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('MongoDB Connected');
+}).catch((err) => {
+  console.log(err,
+    "MongoDB Connection Error. Please make sure MongoDB is running."
+  );
+});
 
-// const User = require('./models/user');
 
 // app.get('/users', async (req, res) => {
 //   try {
@@ -28,15 +31,28 @@ const port = process.env.PORT || 3000;
 //   }
 // });
 
-// app.post('/users', async (req, res) => {
-//   try {
-//     const user = new User(req.body);
-//     await user.save();
-//     res.json(user);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
+app.post('/users', async (req, res) => {
+  try {
+    if (!req.body.name || !req.body.email || !req.body.password) {
+      return res.status(400).json({ message: 'Missing required fields' });
+    }
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({data:user,message:'User created successfully'});    
+  } catch (err) {
+    res.status(500).json({ message: err.message+" internal server error" });
+  }
+});
+
+// -  GET /users - Get all users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
